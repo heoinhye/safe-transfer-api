8b71b1d8f7eefa1ef14eeef7ef66268fe70f3c0d
jane heo <pepupx2@gmail.com>
2026-03-19 15:56:47 -0400

    chore: initial spring boot project setup

---
+++ src/main/java/com/fintech/safetransferapi/SafeTransferApiApplication.java
@@ -0,0 +1,13 @@
+package com.fintech.safetransferapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeTransferApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeTransferApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fintech/safetransferapi/SafeTransferApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.safetransferapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeTransferApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
